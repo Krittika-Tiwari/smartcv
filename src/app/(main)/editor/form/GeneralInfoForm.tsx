ae8e4dd730e06577395210e1237ea2232c695bff
Krittika-Tiwari <krittikatiwari122004@outlook.com>
2025-05-23 21:37:50 +0530

    generalInfo Form

---
+++ src/app/(main)/editor/form/GeneralInfoForm.tsx
@@ -0,0 +1,66 @@
+import { generalInfoSchema, GeneralInfoType } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+export default function GeneralInfoForm() {
+  const form = useForm<GeneralInfoType>({
+    resolver: zodResolver(generalInfoSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+  return (
+    <div className="max-w-xl mx-auto space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">General info</h2>
+        <p className="text-sm text-muted-foreground">
+          This will not appear on your resume.
+        </p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Project name</FormLabel>
+                <FormControl>
+                  <Input placeholder="My cool resume" autoFocus {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Input placeholder="A resume for my next job" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Describe what this resume is for
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+}
